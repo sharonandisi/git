ea6c8bd82c701a2b35b957b61023b49f3ccbf915
sharonandisi <sharonandisi.28@gmail.com>
2018-10-26 15:32:38 +0300

    create form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { RepositoryComponent } from './repository/repository.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { HighlightDirective } from './highlight.directive';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    RepositoryComponent,
+    UserDetailsComponent,
+    HighlightDirective,
+    SearchFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
